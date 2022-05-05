845368b15be097a3fd18acb1f303f0d5f9694e73
Patrik <patrikjo95@gmail.com>
2022-05-05 16:38:51 +0200

    Patrik, Edwin, Toros, Henning
    Fixat constructor i books samt ändrat alla int till Strings

--- src/main/java/com/example/bibliotekbackend/Books/Book.java
+++ src/main/java/com/example/bibliotekbackend/Books/Book.java
@@ -1,17 +1,15 @@
 package com.example.bibliotekbackend.Books;
 
 public class Book {
-    private int ID_book;
+    private String ID_book;
     private String book_title;
-    private int book_qty;
+    private String book_qty;
     private String book_author;
     private String book_genre;
-
-    private int book_year;
-
+    private String book_year;
     private String book_URL;
 
-    public Book(int ID_book, String book_title, int book_qty, String book_author, String book_genre, int book_year, String book_URL) {
+    public Book(String ID_book, String book_title, String book_qty, String book_author, String book_genre, String book_year, String book_URL) {
         this.ID_book = ID_book;
         this.book_title = book_title;
         this.book_qty = book_qty;
@@ -21,7 +19,7 @@ public class Book {
         this.book_URL = book_URL;
     }
 
-    public Book(String book_title, int book_qty, String book_author, String book_genre, int book_year, String book_url) {
+    public Book(String book_title, String book_qty, String book_author, String book_genre, String book_year, String book_url) {
         this.book_title = book_title;
         this.book_qty = book_qty;
         this.book_author = book_author;
@@ -30,11 +28,11 @@ public class Book {
         this.book_URL = book_url;
     }
 
-    public int getID_book() {
+    public String getID_book() {
         return ID_book;
     }
 
-    public void setID_book(int ID_book) {
+    public void setID_book(String ID_book) {
         this.ID_book = ID_book;
     }
 
@@ -46,11 +44,11 @@ public class Book {
         this.book_title = book_title;
     }
 
-    public int getBook_qty() {
+    public String getBook_qty() {
         return book_qty;
     }
 
-    public void setBook_qty(int book_qty) {
+    public void setBook_qty(String book_qty) {
         this.book_qty = book_qty;
     }
 
@@ -70,11 +68,11 @@ public class Book {
         this.book_genre = book_genre;
     }
 
-    public int getBook_year() {
+    public String getBook_year() {
         return book_year;
     }
 
-    public void setBook_year(int book_year) {
+    public void setBook_year(String book_year) {
         this.book_year = book_year;
     }
 

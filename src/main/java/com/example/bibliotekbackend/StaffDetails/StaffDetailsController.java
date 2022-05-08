3a6e300c4d941adc6c301abdd14ec2dd4995e9a0
IsabellaJonsson <isabellajonssonbotkyrka@gmail.com>
2022-05-08 14:03:43 +0200

    Isabella built out staffdetails classes

--- src/main/java/com/example/bibliotekbackend/StaffDetails/StaffDetailsController.java
+++ src/main/java/com/example/bibliotekbackend/StaffDetails/StaffDetailsController.java
@@ -1,7 +1,10 @@
 package com.example.bibliotekbackend.StaffDetails;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class StaffDetailsController {
@@ -9,5 +12,66 @@ public class StaffDetailsController {
     @Autowired
     StaffDetailsService staffDetailsService;
 
+    public StaffDetailsController(StaffDetailsService staffDetailsService) {
+        this.staffDetailsService = staffDetailsService;
+    }
+
+
+    @GetMapping("/insertStaffDetails")
+    public String insertStaffDetails(
+                    @RequestParam(value = "new_staff_firstname") String staff_firstname,
+                    @RequestParam(value = "new_staff_lastname") String staff_lastname,
+                    @RequestParam(value = "new_staff_mail") String staff_mail,
+                    @RequestParam(value = "new_staff_phonenumber") String staff_phonenumber,
+                    @RequestParam(value = "new_staff_adress") String staff_adress,
+                    @RequestParam(value = "new_staff_city") String staff_city,
+                            @RequestParam(value = "new_staff_zip_code") String staff_zip_code,
+                            @RequestParam(value = "new_staff_pnr") String staff_pnr,
+                            @RequestParam(value = "new_staff_ID_staff") int staff_ID_staff
+
+    ) {
+        StaffDetails staffDetails = new StaffDetails(staff_firstname, staff_lastname, staff_mail, staff_phonenumber, staff_adress, staff_city, staff_zip_code, staff_pnr, staff_ID_staff);
+
+        System.out.println("Controller" + staffDetails);
+
+        Map outParameters = staffDetailsService.insertStaffDetails(staffDetails);
+
+        Gson gson = new Gson();
+
+        return gson.toJson(outParameters);
+
+    }
+
+    @PostMapping("/updateStaffDetails")
+    public void updateStaffDetails(
+            @RequestParam(value = "new_staff_ID_staff", defaultValue = "0" ) int staff_ID_staff,
+            @RequestParam(value = "new_staff_firstname") String staff_firstname,
+            @RequestParam(value = "new_staff_lastname") String staff_lastname,
+            @RequestParam(value = "new_staff_mail") String staff_mail,
+            @RequestParam(value = "new_staff_phonenumber") String staff_phonenumber,
+            @RequestParam(value = "new_staff_adress") String staff_adress,
+            @RequestParam(value = "new_staff_city") String staff_city,
+            @RequestParam(value = "new_staff_zip_code") String staff_zip_code,
+            @RequestParam(value = "new_staff_pnr") String staff_pnr
+
+    ) {
+        staffDetailsService.updateStaffDetails(staff_ID_staff, staff_firstname, staff_lastname, staff_mail, staff_phonenumber, staff_adress, staff_city, staff_zip_code, staff_pnr);
+    }
+
+    @DeleteMapping("/deleteStaffDetails")
+    public void deleteStaffDetails(
+            @RequestParam(value = "staff_ID_staff", defaultValue = "-1") int staff_ID_staff) {
+        staffDetailsService.deleteStaffDetails(staff_ID_staff);
+    }
+
+    @GetMapping("/ShowStaffDetailsByID")
+    public String ShowStaffDetailsByID(@RequestParam(value = "staff_ID_staff", defaultValue = "-1") int staff_ID_staff) {
+        return staffDetailsService.ShowStaffDetailsByID(staff_ID_staff);
+    }
+
+    @GetMapping("/ShowAllStaffDetails")
+    public String ShowAllStaffDetails() {
+        return staffDetailsService.showAllStaffDetails();
+    }
 
 }
--- src/main/java/com/example/bibliotekbackend/StaffDetails/StaffDetailsService.java
+++ src/main/java/com/example/bibliotekbackend/StaffDetails/StaffDetailsService.java
@@ -1,13 +1,50 @@
 package com.example.bibliotekbackend.StaffDetails;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 @Service
 public class StaffDetailsService {
 
     @Autowired
     StaffDetailsDao staffDetailsDao;
+    ArrayList<StaffDetails> staffDetailsList;
+    StaffDetails staffDetails;
+
+
+
+    public Map insertStaffDetails(StaffDetails staffDetails) {
+        return staffDetailsDao.insertStaffDetails(staffDetails.getStaff_firstname(), staffDetails.getStaff_lastname(), staffDetails.getStaff_mail(), staffDetails.getStaff_phonenumber(), staffDetails.getStaff_adress(), staffDetails.getStaff_city(), staffDetails.getStaff_zip_code(), staffDetails.getStaff_pnr(), staffDetails.getStaff_ID_staff());
+    }
+
+    public void updateStaffDetails(int staff_ID_staff, String staff_firstname, String staff_lastname, String staff_mail, String staff_phonenumber, String staff_adress, String staff_city, String staff_zip_code, String staff_pnr) {
+        staffDetailsDao.updateStaffDetails(staff_ID_staff, staff_firstname, staff_lastname, staff_mail, staff_phonenumber, staff_adress, staff_city, staff_zip_code, staff_pnr);
+
+    }
+
+    public void deleteStaffDetails(int staff_ID_staff) {
+        staffDetailsDao.deleteStaffDetails(staff_ID_staff);
+    }
+
+    public String ShowStaffDetailsByID(int staff_ID_staff) {
+        Gson gson = new Gson();
+        staffDetails = staffDetailsDao.ShowStaffDetailsByID(staff_ID_staff);
+        String staffDetailString = gson.toJson(staffDetails);
+        return staffDetailString;
+
+
+    }
+
+    public String showAllStaffDetails() {
+        Gson gson = new Gson();
+        staffDetailsList = staffDetailsDao.showAllStaffDetails();
+        String staffDetailString = gson.toJson(staffDetailsList);
+        return staffDetailString;
+    }
 
 
 }

73c4ad863c995473069ba74b4501833c43e69df1
Muhammed Yilmaz <Muhammed.y00@outlook.com>
2022-06-01 16:30:31 +0200

    Worked with Room classes

--- src/main/java/com/example/bibliotekbackend/Rooms/Room.java
+++ src/main/java/com/example/bibliotekbackend/Rooms/Room.java
@@ -1,20 +1,40 @@
 package com.example.bibliotekbackend.Rooms;
 
 public class Room {
-    private int ID_room;
+    private String room_name_live;
+    private String ID_room;
     private String room_name;
+    private String date_pick;
+    private String time_in;
+    private String customer_id;
 
 
-    public Room(int ID_room, String room_name) {
+
+    public Room(String ID_room, String room_name) {
         this.ID_room = ID_room;
         this.room_name = room_name;
     }
+    public Room( String ID_room) {
+        this.ID_room = ID_room;
+    }
 
-    public int getID_room() {
+
+    public Room(String room_name_live, String date_pick, String time_in, String customer_pnr) {
+        this.room_name_live = room_name_live;
+        this.date_pick = date_pick;
+        this.time_in = time_in;
+        this.customer_id = customer_id;
+
+    }
+
+
+
+
+    public String getID_room() {
         return ID_room;
     }
 
-    public void setID_room(int ID_room) {
+    public void setID_room(String ID_room) {
         this.ID_room = ID_room;
     }
 
@@ -25,6 +45,18 @@ public class Room {
     public void setRoom_name(String room_name) {
         this.room_name = room_name;
     }
+
+    public String getRoom_name_live() {return room_name_live;}
+    public void setRoom_name_live(String room_name_live) {this.room_name_live = room_name_live;}
+
+    public String getDate_pick() {return date_pick;}
+    public void setDate_pick(String date_pick) {this.date_pick = date_pick; }
+
+    public String getTime_in() {return time_in;}
+    public void setTime_in(String time_in) { this.time_in = time_in;}
+
+    public String getCustomer_id() {return customer_id;}
+    public void setCustomer_id(String customer_id) {this.customer_id = customer_id;}
 
     @Override
     public String toString() {
--- src/main/java/com/example/bibliotekbackend/Rooms/RoomController.java
+++ src/main/java/com/example/bibliotekbackend/Rooms/RoomController.java
@@ -1,8 +1,11 @@
 package com.example.bibliotekbackend.Rooms;
 
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class RoomController {
@@ -14,36 +17,24 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    /**
-     * Sends request to insert a new room to database
-     */
-    @PostMapping("/insertRoom")
-    public void insertRoom(@RequestParam(value = "room_name", defaultValue = "noRoom_name") String room_name) {
-        roomService.insertRoom(room_name);
-    }
+
 
     /**
      * Sends request to update a new room in the database, specify ID
      */
     @PostMapping("/updateRoom")
-    public void updateRoom(@RequestParam(value = "ID_room", defaultValue = "0") int ID_room,
+    public void updateRoom(@RequestParam(value = "ID_room", defaultValue = "0") String ID_room,
                            @RequestParam(value = "room_name", defaultValue = "noRoom_name") String room_name) {
         roomService.updateRoom(ID_room, room_name);
     }
 
-    /**
-     * Sends request to delete a room from database by ID
-     */
-    @DeleteMapping("/deleteRoomByID")
-    public void deleteRoom(@RequestParam(value = "ID_room", defaultValue = "-1") int ID_room) {
-        roomService.deleteRoom(ID_room);
-    }
+
 
     /**
      * Sends request to download one room from database by ID
      */
     @GetMapping("/downloadOneRoom")
-    public String downloadOneRoom(@RequestParam(value = "ID_room", defaultValue = "-1") int ID_room) {
+    public String downloadOneRoom(@RequestParam(value = "ID_room", defaultValue = "-1") String ID_room) {
         return roomService.downloadOneRoomByID(ID_room);
     }
 
@@ -59,4 +50,40 @@ public class RoomController {
     public String downloadAllRooms() {
         return roomService.downloadAllRooms();
     }
+
+    @GetMapping ("/one_month_calender")
+    public String one_month_calender() {
+
+        Map outParameters = roomService.one_month_calender();
+
+        Gson gson = new Gson();
+        return gson.toJson(outParameters);
+    }
+
+    @GetMapping ("/Room_calender")
+    public String Room_calender(@RequestParam(value = "room_name_live")String room_name_live
+    ) {
+        Room room = new Room(room_name_live);
+        Map outParameters = roomService.Room_calender(room_name_live);
+
+        Gson gson = new Gson();
+        return gson.toJson(outParameters);
+        
+    }
+
+    @GetMapping ("/pick_a_room")
+    public String pick_a_room(@RequestParam(value = "room_name_live")String room_name_live,
+                              @RequestParam(value = "date_pick") String date_pick,
+                              @RequestParam(value = "time_in") String time_in,
+                              @RequestParam(value = "customer_id")String customer_id) {
+        Room room = new Room(room_name_live,date_pick,time_in,customer_id);
+        Map outParameters = roomService.pick_a_room(room);
+
+        Gson gson = new Gson();
+        return gson.toJson(outParameters);
+    }
+
+
+
+
 }
